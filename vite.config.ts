882f2b65cc60572de4bb558593bdba41b9ab5f89
zainudinsainal <supernova37@gmail.com>
2021-09-02 19:35:58 +0800

    Add gzip

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import VuePlugin from '@vitejs/plugin-vue'
+import { brotliCompressSync } from "zlib"
+import gzipPlugin from "rollup-plugin-gzip"
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-    VuePlugin()
+    VuePlugin(),
+    gzipPlugin(),
+    gzipPlugin({
+      customCompression: (content) => brotliCompressSync(Buffer.from(content)),
+      fileName: ".br",
+    })
   ],
 })
