e6f0170727310e349b2b88bd779033baed902c4f
zainudinsainal <supernova37@gmail.com>
2021-09-02 15:05:34 +0800

    Add job

--- app/jobs/home_job.rb
+++ app/jobs/home_job.rb
@@ -2,6 +2,6 @@ class HomeJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    puts 'Job Executed'
+    Product.create(name: 'Test Sidekiq', price: 20, description: 'Testing 1 2 3...')
   end
 end
