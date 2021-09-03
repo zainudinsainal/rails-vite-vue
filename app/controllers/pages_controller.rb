ae6df6bc73f682d8c11d482df279298ed39a59f8
zainudinsainal <supernova37@gmail.com>
2021-09-03 19:26:45 +0800

    push again

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    # gon.user = { name: 'John', role: 'Admin', id: '2' }
+    gon.user = { name: 'John', role: 'Admin', id: '2' }
   end
 end
