2a17e3c61c3f881f0ed0d4bd8de45b5c1fa201cb
zainudinsainal <supernova37@gmail.com>
2021-09-08 11:29:11 +0800

    Test with quasar

--- app/javascript/src/main.js
+++ app/javascript/src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { initQuasar } from './plugins/quasar'
 
 export default () => {
   document.addEventListener('DOMContentLoaded', () => {
     const app = createApp(App);
+    initQuasar(app)
     app.use(store)
     app.use(router)
     app.mount('#app')
