14a047ffba212afdeb5cf7e486857004746aa99d
zainudinsainal <supernova37@gmail.com>
2022-03-03 19:32:46 +0800

    Change title

--- app/javascript/src/components/layouts/header/Header.vue
+++ app/javascript/src/components/layouts/header/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand navbar-light bg-light">
     <div class="container">
-      <router-link to="/" class="navbar-brand"># Vue</router-link>
+      <router-link to="/" class="navbar-brand">VueOnRails</router-link>
       <div class="navbar-nav mr-auto">
         <li class="nav-item">
           <router-link to="/products" class="nav-link">
